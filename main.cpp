979442a2701ac37b24dfc22528309bf99148b83e
Jimmy Leta <jimmy.leta@gmail.com>
2021-06-20 21:44:24 -0400

    created histogram example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
+#include <cstdlib>
+#include <random>
+
 #include "matplotlibcpp.h"
+
 namespace plt = matplotlibcpp;
-int main() {
-  plt::plot({1, 3, 2, 4});
+
+int main(int argc, char *argv[]) {
+
+  int npoints = std::atoi(argv[1]);
+
+  std::random_device device{};
+  std::mt19937 rng{device()};
+  std::normal_distribution<> normal_dist{0, 1};
+  std::vector<double> v;
+
+  for (int i = 0; i < npoints; ++i) {
+    v.push_back(normal_dist(rng));
+  }
+
+  plt::hist(v);
+  plt::xlabel("xdata");
+  plt::grid(true);
   plt::show();
 }
