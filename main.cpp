acdbe48de22d5d24abf22550f1b96365721ba254
Jimmy Leta <jimmy.leta@gmail.com>
2021-07-01 22:37:58 -0400

    updated demo problem

--- main.cpp
+++ main.cpp
@@ -11,15 +11,31 @@ int main(int argc, char *argv[]) {
 
   std::random_device device{};
   std::mt19937 rng{device()};
-  std::normal_distribution<> normal_dist{0, 1};
-  std::vector<double> v;
+  std::normal_distribution<double> normal_dist1{0, 1};
+  std::normal_distribution<double> normal_dist2{1, 2};
+  std::vector<double> x;
+  std::vector<double> y;
+  std::vector<double> z;
 
   for (int i = 0; i < npoints; ++i) {
-    v.push_back(normal_dist(rng));
+    auto xsample = normal_dist1(rng);
+    auto ysample = normal_dist2(rng);
+    auto zsample = xsample + 5.0 * ysample;
+    x.push_back(xsample);
+    y.push_back(ysample);
+    z.push_back(zsample);
   }
 
-  plt::hist(v);
-  plt::xlabel("xdata");
-  plt::grid(true);
+  plt::figure();
+
+  plt::subplot(3, 1, 1);
+  plt::hist(x);
+
+  plt::subplot(3, 1, 2);
+  plt::hist(y);
+
+  plt::subplot(3, 1, 3);
+  plt::hist(z);
+
   plt::show();
 }
